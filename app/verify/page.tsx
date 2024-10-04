e8d828094feea2d1b291628e092405d18654c726
donjne <davidjrn247@gmail.com>
2024-10-04 07:40:08 +0100

    added reclaim protocol for verification

---
+++ app/verify/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+import React from "react";
+import ReclaimVerificationUI from "@/components/reclaim/Verify";
+
+const VerifyPage: React.FC = () => {
+  return <ReclaimVerificationUI />;
+};
+
+export default VerifyPage;
