79d529349a413ea07b1de4d46fe9eefddfc12b6d
donjne <davidjrn247@gmail.com>
2024-10-04 03:27:44 +0100

    escrow ui done

---
+++ app/escrow/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+import React from "react";
+import EscrowUI from "@/components/escrow/Escrow";
+
+const EscrowPage: React.FC = () => {
+  return <EscrowUI />;
+};
+
+export default EscrowPage;
---
+++ components/chat/Squads.tsx
@@ -0,0 +1,10 @@
+
+
+
+// 'use client'
+
+// import React, { useState, useEffect, useCallback } from 'react';
+// import { useWallet } from '@solana/wallet-adapter-react';
+// import { Connection, PublicKey, SystemProgram, LAMPORTS_PER_SOL, TransactionMessage, Transaction, sendAndConfirmTransaction } from '@solana/web3.js';
+// import * as multisig from "@sqds/multisig";
+// import { toast } from 'react-toastify';
